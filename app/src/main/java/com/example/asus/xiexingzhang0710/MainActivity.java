ea9ca0275ea1094ed185e33bbec0ac8be6d5fc42
xxz <xiedi315@163.com>
2017-07-10 09:36:00 +0800

    完成提交

--- app/src/main/java/com/example/asus/xiexingzhang0710/MainActivity.java
+++ app/src/main/java/com/example/asus/xiexingzhang0710/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.asus.xiexingzhang0710;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private AmountView mAmountView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAmountView = (AmountView) findViewById(R.id.amount_view);
+        mAmountView.setGoods_storage(50);
+        mAmountView.setOnAmountChangeListener(new AmountView.OnAmountChangeListener() {
+            @Override
+            public void onAmountChange(View view, int amount) {
+                Toast.makeText(getApplicationContext(), "Amount=>  " + amount, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
